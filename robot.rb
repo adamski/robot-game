cc91ad1a00ec7c5377fa9abcd22aaffb3aa8f956
Adam Wilson <adam@codegarden.co.uk>
2014-12-12 13:04:07 +0000

    add attr_reader for status

--- robot.rb
+++ robot.rb
@@ -1,6 +1,7 @@
 require './board.rb'
 
 class Robot
+  attr_reader :status
 
   #@@directions = { up: {y: -1}, right: {x: 1}, down: {y: 1}, left: {x: -1} }
   @@directions = [ :up, :right, :down, :left ]
@@ -26,6 +27,7 @@ class Robot
       self.send(command)
       @moves += 1
       check_status
+      report
     end
   end
 
